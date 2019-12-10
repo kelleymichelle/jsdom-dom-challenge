0b284d405fb70cd10a8893e8acbef791536b4ea7
Kelley Chaplain <kmchaplain@gmail.com>
2019-12-10 13:47:56 -0600

    Done.

--- challenge.js
+++ challenge.js
@@ -2,7 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
   const counterId = countCounter();
   const pauseBtn = document.getElementById('pause')
   pauseBtn.addEventListener('click', function() {
-    pauseCounter(counterId);
+    if (pauseBtn.innerText === "pause") {
+      pauseCounter(counterId);
+    } else {
+      resumeCounter();  
+    }
   })
   const addBtn = document.getElementById('plus')
   addBtn.addEventListener('click', function() {
@@ -12,8 +16,24 @@ document.addEventListener('DOMContentLoaded', function() {
   minusBtn.addEventListener('click', function() {
     minusCounter();
   })
+  const form = document.getElementById('comment-form')
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log(event.target["0"].form["0"].form["0"].nextSibling.previousElementSibling.value)
+    const comment = event.target["0"].form["0"].form["0"].nextSibling.previousElementSibling.value
+    addComment(comment);
+  })
   
 });
+
+function addComment(comment) {
+  listDiv = document.getElementById('list')
+  const p = document.createElement('p')
+  p.innerText = comment
+  listDiv.appendChild(p)
+  const commentForm = document.getElementById('comment-form')
+  commentForm.reset();
+}
 
 function countCounter() {
   return setInterval(() => {
@@ -25,9 +45,22 @@ function countCounter() {
 };
 
 function pauseCounter(counterId) {
-  console.log("clicked", counterId);
+  console.log("paused", counterId, event);
+  const pauseBtn = document.getElementById("pause")
+  pauseBtn.innerText = "Resume"
   clearInterval(counterId);
+  disableButtons();
 };
+
+function resumeCounter() {
+  const pauseBtn = document.getElementById('pause')
+  pauseBtn.addEventListener('click', function() {
+    console.log("Resume counter");
+    pauseBtn.innerText = "pause"
+    reenableButtons();
+    countCounter();
+  })
+}
 
 function addCounter() {
   const count = document.getElementById('counter')
@@ -41,4 +74,26 @@ function minusCounter() {
   let num = parseInt(count.innerText)
   num--
   count.innerText = num
+}
+
+function disableButtons() {
+  const minusBtn = document.getElementById("minus")
+  const plusBtn = document.getElementById("plus")
+  const heartBtn = document.getElementById("heart")
+  const submitBtn = document.getElementById("submit")
+  minusBtn.disabled = true
+  plusBtn.disabled = true
+  heartBtn.disabled = true
+  submitBtn.disabled = true
+}
+
+function reenableButtons() {
+  const minusBtn = document.getElementById("minus")
+  const plusBtn = document.getElementById("plus")
+  const heartBtn = document.getElementById("heart")
+  const submitBtn = document.getElementById("submit")
+  minusBtn.disabled = false
+  plusBtn.disabled = false
+  heartBtn.disabled = false
+  submitBtn.disabled = false
 }
